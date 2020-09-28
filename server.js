320666f0fcd5bab746678c0cb3c37994b4ef86b7
JQueen <queen_jenf@yahoo.com>
2020-09-27 18:37:54 -0700

    add heroku

--- server.js
+++ server.js
@@ -52,5 +52,5 @@ function filterByQuery(query, animalsArray) {
 
 
 app.listen(3001, () => {
-    console.log(`API server now on port 3001!`);
+    console.log(`API server is on port 3001!`);
   });
